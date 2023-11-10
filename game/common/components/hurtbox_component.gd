c91adab36594fef660c7154675da4deb12d1ab10
YasserHage <yasser.hage1@gmail.com>
2023-11-10 19:29:23 -0300

    Fix engine crash

--- game/common/components/hurtbox_component.gd
+++ game/common/components/hurtbox_component.gd
@@ -1,11 +1,16 @@
 class_name HurtBoxComponent extends Area2D
 
+signal entered_death_zone()
 signal hurt_box_hit(hit_box:HitBoxComponent)
+
+@export
+var death_zone_layer: int
 
 func _ready():
 	self.area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area):
-	area.                      
-#	if area is HitBoxComponent:
-#		hurt_box_hit.emit(area)
+	if area.get_collision_layer_value(8):
+		entered_death_zone.emit()
+	if area is HitBoxComponent:
+		hurt_box_hit.emit(area)
