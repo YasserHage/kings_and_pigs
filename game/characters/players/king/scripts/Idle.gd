8523378ec8e23d69cf8b0b4ed82e62d827fc9065
YasserHage <yasser.hage1@gmail.com>
2023-11-09 22:01:17 -0300

    Functional one way platforms

--- game/characters/players/king/scripts/Idle.gd
+++ game/characters/players/king/scripts/Idle.gd
@@ -8,8 +8,11 @@ var jump_state: State
 var attack_sprite: Sprite2D
 @export
 var attack_animation_name: String
+@export
+var one_way_layer: int
 
 var isAttacking: bool = false
+var resetOneWayLayer: bool = false
 
 func enter() -> void:
 	super()
@@ -24,6 +27,8 @@ func processInput(event: InputEvent) -> State:
 	
 func processFrame(delta: float) -> State:
 	_verifyAttackFinished()
+	if resetOneWayLayer:
+		parent.set_collision_mask_value(one_way_layer, true)
 	return _handle_input()
 	
 func processPhysics(delta: float) -> State:
@@ -37,6 +42,10 @@ func processPhysics(delta: float) -> State:
 func _handle_input() -> State:
 	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
 		return walk_state
+	if Input.is_action_pressed("ui_down"):
+		resetOneWayLayer = true
+		parent.set_collision_mask_value(one_way_layer, false)
+		return null
 	if Input.is_action_just_pressed("jump"):
 		return jump_state
 	if Input.is_action_pressed("attack"):
