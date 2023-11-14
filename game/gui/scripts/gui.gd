9de6c532319d83ba2b56440d62196e1edaa43583
YasserHage <yasser.hage1@gmail.com>
2023-11-13 21:02:31 -0300

    Change button hover texture

--- game/gui/scripts/gui.gd
+++ game/gui/scripts/gui.gd
@@ -4,6 +4,7 @@ extends CanvasLayer
 var pause_menu: Control
 
 func _ready():
+	pause_menu.visible = false
 	GameplayController.game_paused.connect(toogle_pause_menu)
 	
 func toogle_pause_menu(is_paused: bool):
